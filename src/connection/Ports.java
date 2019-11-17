9c56e8d81d03e8da7e8d5ba046201579c96b1fa1
Felipeb61 <felipe.batistela61@gmail.com>
2019-11-17 02:44:27 -0300

    add upload port

--- src/connection/Ports.java
+++ src/connection/Ports.java
@@ -4,7 +4,8 @@ public enum Ports {
     HEALTHCHECK_1(4000),
     HEALTHCHECK_2(4001),
     DOWNLOAD(4002),
-    REMOVE(4003);
+    REMOVE(4003),
+    UPLOAD(4004);
     
     private final int value;
     
